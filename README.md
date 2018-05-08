b7dcfb84f055f24e8c9244abd5d2c860a3050e77
rb <ravbahra@gmail.com>
2018-05-08 11:24:14 +0100

    base vagrant starter

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # vagrant-chocolatey-base
+
+This base vagrant file will get you started with chocolatey. It creates a Windows server 2012 R2 image, installs chocolatey from the powershell script included and then installs google chrome from the chocolatey repository.
---
+++ install_apps.ps1
@@ -0,0 +1,5 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('http://gb0015chocolatey.blucube.aberdeen.enet.slb.com/install.ps1'));
+
+# some code hidden removed references hostnames from my office
+
+choco install googlechrome -y;
